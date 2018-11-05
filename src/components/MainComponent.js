7d294d8c2c8f2e2161c298c9cc48bffcaf798672
cca2016 <349563145@qq.com>
2018-11-05 12:26:40 -0500

    aboutus

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
+import About from './AboutComponent'
 import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Home from './HomeComponent';
@@ -48,6 +49,7 @@ class Main extends Component {
         <Header />
           <Switch>
               <Route path='/home' component={HomePage} />
+              <Route path='/aboutus' component={() => <About leaders={this.state.leaders}/>}/>
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />} />
