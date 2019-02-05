19722c1eece9f47ccb1c29e60b67f90145a51d10
陈昶安 <chenchangan@chenchangandeMacBook-Pro.local>
2019-02-05 15:28:57 -0800

    Updates

---
+++ precache-manifest.a8556939ea890e16a15d0e0326716f3b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "618bde4c45b823cc8421",
+    "url": "/Restaurant/static/css/main.490a4c62.chunk.css"
+  },
+  {
+    "revision": "618bde4c45b823cc8421",
+    "url": "/Restaurant/static/js/main.618bde4c.chunk.js"
+  },
+  {
+    "revision": "3d74e27d6d46addf2446",
+    "url": "/Restaurant/static/css/1.e4d15ce2.chunk.css"
+  },
+  {
+    "revision": "3d74e27d6d46addf2446",
+    "url": "/Restaurant/static/js/1.3d74e27d.chunk.js"
+  },
+  {
+    "revision": "af2006c8aa01dbc5c88b",
+    "url": "/Restaurant/static/js/runtime~main.af2006c8.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Restaurant/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Restaurant/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Restaurant/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Restaurant/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Restaurant/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "c1820df201f88ee0d0a5dbf96896a465",
+    "url": "/Restaurant/index.html"
+  }
+];
